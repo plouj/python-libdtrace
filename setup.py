2ba85aa8c2e9782ecfea286d9b6b946bbd8a029f
Michael Ploujnikov <michaelp@oanda.com>
2011-03-31 09:02:23 -0400

    allow building without cython

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-ext_modules = [
-    Extension("dtrace",
-              ["dtrace.pyx"],
-              libraries=["dtrace"]) # Unix-like specific
-]
+mycmdclass = {}
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    print "cython not found, using a previously-cython'd .c file."
+    ext_modules = [
+        Extension("dtrace",
+                  ["dtrace.c"],
+                  libraries=["dtrace"])
+        ]
+else:
+    ext_modules = [
+        Extension("dtrace",
+                  ["dtrace.pyx"],
+                  libraries=["dtrace"])
+        ]
+    mycmdclass['build_ext'] = build_ext
 
 setup(
-  name = 'Dtrace Python wrapper',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
+    name = 'Dtrace Python wrapper',
+    cmdclass = mycmdclass,
+    ext_modules = ext_modules
+    )
